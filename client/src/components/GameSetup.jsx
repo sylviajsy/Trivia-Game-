56cdee8483fb559591e3792341b91c42efefdffe
Siyi Ji <sylviajsyy@gmail.com>
2026-02-04 00:12:15 -0600

    Add selection for type and difficulty

--- client/src/components/GameSetup.jsx
+++ client/src/components/GameSetup.jsx
@@ -24,11 +24,23 @@ const GameSetup = ({ onSubmit }) => {
         <h2>Game Settings</h2>
         <form onSubmit={handleSubmit}>
             <label>Number of questions</label>
-            <input type="number" required placeholder="Number of questions" ref={noQuestion}/>
+            <input type="number" required placeholder="Number of questions" defaultValue="10" ref={noQuestion}/>
 
             <label>Type</label>
+            <select ref={userType} defaultValue="">
+                <option value="">Any</option>
+                <option value="multiple">Multiple Choice</option>
+                <option value="boolean">True / False</option>
+            </select>
 
             <label>Difficulty</label>
+            <select ref={userDifficulty} defaultValue="">
+                <option value="">Any Difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+            </select>
+
             <label>Category</label>
             <button type="submit">Start Game</button>
         </form>
