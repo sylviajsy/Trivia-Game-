ed3006fb8734579fcf869070b36842251b5363c3
Siyi Ji <sylviajsyy@gmail.com>
2026-02-04 13:58:20 -0600

    Using decode to remove HTML entities

--- client/src/components/GamePlay.jsx
+++ client/src/components/GamePlay.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const GamePlay = ({ question }) => {
+    function decodeHtml(str) {
+        const doc = new DOMParser().parseFromString(str, "text/html");
+        return doc.documentElement.textContent;
+        }
+
   return (
     <div>
       <ol>
         {question.map((q, i) => (
-          <li key={i}>{q.question}</li>
+          <li key={i}>{decodeHtml(q.question)}</li>
         ))}
       </ol>
     </div>
