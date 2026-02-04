ac965f5ac8558438e70525fc0f7da5f99db369cc
Siyi Ji <sylviajsyy@gmail.com>
2026-02-03 21:10:29 -0600

    Set up a GET route in Express to fetch trivia questions from the external API

--- server/index.js
+++ server/index.js
@@ -5,8 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (req, res) => {
-  res.json({ ok: true });
+app.get("/questions", async(req, res) => {
+    try {
+        const response = await fetch(`https://opentdb.com/api.php?amount=10`);
+        const data = await response.json();
+        console.log("Fetched data for:", data);
+        res.json(data);
+    } catch (error){
+        console.error(error);
+        res.status(500).json({ error: "Error fetching data" });
+    }
 });
 
 const PORT = process.env.PORT || 8080;
